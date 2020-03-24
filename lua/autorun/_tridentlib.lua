0514b816426a0bf20f3ec9a80350036c7602a35a
NERO2k <nero2k@outlook.com>
2020-03-24 17:46:05 +0100

    Maybe done?

--- lua/autorun/_tridentlib.lua
+++ lua/autorun/_tridentlib.lua
@@ -1,12 +1,7 @@
- -- TRIDENTLIB 3.0                                  
-if SERVER then util.AddNetworkString("tridentlib_load_orders") end
-  
+ -- TRIDENTLIB 4.0                                    
 _tridentlib = _tridentlib or {}     
 _tridentlib.metaTables = {}  
 
-_tridentlib.FileLoader = {}
-_tridentlib.FileLoader.Data = {} 
-_tridentlib.FileLoader.Config = {}
 _tridentlib.MetaTables = {}
 _tridentlib.MetaFunctions = {}
 
@@ -14,7 +9,7 @@ _tridentlib.FunctionLoader = _tridentlib.FunctionLoader or {}
 _tridentlib.FunctionLoader.Functions = _tridentlib.FunctionLoader.Functions or {}
 
 --[ CONSOLE PRINT ]  
-function _tridentlib.ConsolePrint(data, message)
+function _tridentlib.ConsolePrint(message)
 	print(message)
 end
 
@@ -62,178 +57,51 @@ function _tridentlib.RunFunction(func, self, ...)
 end
 
 --[ LOAD ADDDON INT ] 
-function _tridentlib.LoadAddonInt(data, name)
-	_tridentlib.ConsolePrint(data, " ")
-	_tridentlib.ConsolePrint(data, "TRIDENTLIB> Loading Addon: "..name..".")
-	if data["func"] then
-		_tridentlib.ConsolePrint(data, " > Createing addon functions")
-		local inject = [[
-		local function DefineFunction(name, func, metatables)
-			_tridentlib.DefineFunction("%s::"..name, func, metatables)
-		end
-		_tridentlib.DefineFunction("%s::DefineFunction", DefineFunction, {})
-
-		function %s(func, ...)
-			return _tridentlib.RunFunction("%s::"..func, nil, ...)
-		end
-		]]
-		local func, err = CompileString(string.format(inject, data["func"], data["func"], data["func"], data["func"], data["func"], data["func"]), "TRIDENTLIB::Function Definer", false)
-		if err then error("TRIDENTLIB::Function Definer | Fatal Error "..data["func"].."/"..data["folder"], 2) 
-		else _tridentlib.ConsolePrint(data, " > Finalized creation of functons.") end
-		func()
+function _tridentlib.LoadAddonInt(name, namespace, enable_namespace)
+	_tridentlib.ConsolePrint(" ")
+	_tridentlib.ConsolePrint("TRIDENTLIB> Loading Addon: "..name..".")
+	_tridentlib.ConsolePrint(" > Initializing addon.")
+	local inject = [[
+	local function DefineFunction(name, func, metatables)
+		_tridentlib.DefineFunction("%s::"..name, func, metatables)
 	end
-	if data["folder"] then
-		_tridentlib.FileLoader.Data[data["folder"]] = {}
-		_tridentlib.FileLoader.mapStart(data["folder"], data["folder"].."/")
-		_tridentlib.FileLoader.Config[data["folder"]] = data
+	_tridentlib.DefineFunction("%s::DefineFunction", DefineFunction, {})
 
-		table.sort( _tridentlib.FileLoader.Data[data["folder"]], function( a, b ) return a["priority"] < b["priority"] end )
-		print(" > Loading "..#_tridentlib.FileLoader.Data[data["folder"]].." files.")
-		for k, v in pairs(_tridentlib.FileLoader.Data[data["folder"]]) do
-			_tridentlib.FileLoader.loadFile(data, v)
-		end
-
-		--[ START LOAD ORDERS ] 
-		net.Start("tridentlib_load_orders")
-		local dat = {} dat[data["folder"]] = _tridentlib.FileLoader.Data[data["folder"]]
-		net.WriteTable({ dat, _tridentlib.FileLoader.Config })
-		net.Broadcast(ply)
+	function %s(func, ...)
+		return _tridentlib.RunFunction("%s::"..func, nil, ...)
+	end
+	]]
+	if (enable_namespace == "true") then
+		local func, err = CompileString(string.format(inject, namespace, namespace, namespace, namespace, namespace, namespace), "TRIDENTLIB::Function Definer", false)
+		if err then error("TRIDENTLIB::Function Definer | Fatal Error loading "..name.."/"..namespace, 2) 
+		else func() _tridentlib.ConsolePrint(" > Enabled namespace functions.") end
 	end
 end
 
 if SERVER then
-
---[ PARSE FILE ] 
-function _tridentlib.FileLoader.parseFile(raw, full)
-	local data = {}
-	data["priority"] = 99999999999
-	local prefix = string.Explode("_", raw)[1]
-	local suffix = string.Explode(".",raw)[2]
-	if suffix == "lua" then
-		data["prefix"] = data["prefix"] or prefix
-	    // check file contents
-	    local filedata = file.Read(full, "LUA")
-	    if filedata then
-	    	local stage1 = string.Explode("--[[tridentlib", filedata)
-	    	if stage1[2] then
-	    		local stage2 = string.Explode("--tridentlib]]", stage1[2])
-	    		local filecontents = util.JSONToTable("{"..stage2[1].."}")
-	    		if filecontents then
-	    			data["prefix"] = filecontents["state"] or data["prefix"]
-	    			data["priority"] = filecontents["priority"] or data["priority"]
-	    			data["data"] = filecontents
-	    		end
-	    	end
-	    end
+	function _tridentlib.defineAddon(name, namespace, enable_namespace)
+		_tridentlib.LoadAddonInt(name, namespace, enable_namespace)
 	end
-	return data
-end
-
---[ MAP FOLDER ] 
-function _tridentlib.FileLoader.mapFolder(base, folder) 
-	local files, folders = file.Find( folder.."/*", "LUA") 
-	for k, v in pairs(files) do 
-		table.insert(_tridentlib.FileLoader.Data[base], table.Merge({ full = folder.."/"..v, raw = v }, _tridentlib.FileLoader.parseFile(v, folder.."/"..v)))
-	end 
-	for k, v in pairs(folders) do 
-		if (!string.StartWith(v, "-")) then
-			_tridentlib.FileLoader.mapFolder(base, folder.."/"..v) 
-		end
-	end 
-end 
-
---[ START FOLDER MAPPING ] 
-function _tridentlib.FileLoader.mapStart(base, folder)
-	local base_files, folder_table = file.Find(folder .. "*", "LUA")
-	for k, v in pairs(base_files) do
-		table.insert(_tridentlib.FileLoader.Data[base], table.Merge({ full = folder..v, raw = v }, _tridentlib.FileLoader.parseFile(v, folder..v)))
-	end 
-	for k, v in pairs(folder_table) do
-		if (!string.StartWith(v, "-")) then
-			_tridentlib.FileLoader.mapFolder(base, folder..v) 
-		end
+	function _tridentlib.loadFile(file, name, state, priority)
+		if (state === "client") then AddCSLuaFile(file) end
+		if (state === "shared") then AddCSLuaFile(file) require(file) end
+		if (state === "server") then require(file) end
+		print(" > Loaded "..name)
 	end
-end 
-
---[ LOAD FILE ] 
-function _tridentlib.FileLoader.loadFile(data, filex)
-	if filex["prefix"] then
-		local state = string.lower(filex["prefix"])
-		if(state == "cl" or state == "client")then 
-			if SERVER then AddCSLuaFile(filex["full"]) end 
-		end 
-		if(state == "sv" or state == "server" or state == "sh" or state == "shared")then 
-			if SERVER then 
-				include(filex["full"]) 
-				if (state == "sh" or state == "shared") then AddCSLuaFile(filex["full"]) end
-				if filex["data"] and filex["data"]["name"] then
-					_tridentlib.ConsolePrint(data, "  > Loaded: ".. filex["data"]["name"])
-				else
-					_tridentlib.ConsolePrint(data, "  > Loaded: ".. filex["raw"])
-				end
-			end 
-		end
-	else
-		print("  > File without state found. Skipping.")
+	function _tridentlib.finalizeDefine(name, namespace, enable_namespace)
+		print("> Finished loading "..name)
 	end
 end
 
---[ START ADDON LOAD ] 
-function _tridentlib.LoadAddon(data)
-	local nme = data["folder"] or data["name"]
-	_tridentlib.LoadAddonInt(data, nme)
-	_tridentlib.ConsolePrint(data, " > Loaded Addon")
-
-	_tridentlib.ConsolePrint(data, " ")
-end
-
---[ LOAD TRIDENTLIB ] 
-_tridentlib.LoadAddon({ folder = "tridentlib", func = "tridentlib" })
-
---[ LOAD UNLOADED ADDONS ] 
-if tridentlib_load then
-	for k,v in pairs(tridentlib_load) do
-		_tridentlib.LoadAddon(v)
+if CLIENT then
+	function _tridentlib.defineAddon(name, namespace, enable_namespace)
+		_tridentlib.LoadAddonInt(name, namespace, enable_namespace)
+	end
+	function _tridentlib.loadFile(file, name, state, priority)
+		if (state === "client" or state === "shared") then require(file) end
+		print(" > Loaded "..name)
+	end
+	function _tridentlib.finalizeDefine(name, namespace, enable_namespace)
+		print("> Finished loading "..name)
 	end
 end
-
---[ SEND FILES ON CONNECT ] 
-hook.Add("PlayerInitialSpawn","tridentlib_load_orders",function(ply)
-	local datZ = {} datZ["tridentlib"] = _tridentlib.FileLoader.Data["tridentlib"]
-	local datX = _tridentlib.FileLoader.Data datX["tridentlib"] = nil
-	
-	net.Start("tridentlib_load_orders")
-	net.WriteTable({ datZ, _tridentlib.FileLoader.Config })
-	net.Send(ply) 
-
-	net.Start("tridentlib_load_orders")
-	net.WriteTable({ datX, _tridentlib.FileLoader.Config })
-	net.Send(ply)
-end)
-
-else
-	function _tridentlib.LoadAddon(data) end
-	net.Receive("tridentlib_load_orders",function()
-		local datax = net.ReadTable()
-		local data = datax[1] local conf = datax[2]
-		for k, v in pairs(data) do
-			if conf[k] then
-				local data = { func = conf[k]["func"] }
-				_tridentlib.LoadAddonInt(data, k)
-				table.sort( v, function( a, b ) return a["priority"] < b["priority"] end )
-				for k,v in pairs(v) do
-					v["prefix"] = string.lower(v["prefix"])
-					if(v["prefix"] == "cl" or v["prefix"] == "client" or v["prefix"] == "sh" or v["prefix"] == "shared") then 
-						include(v["full"]) 
-						if v["data"] and v["data"]["name"] then
-							_tridentlib.ConsolePrint(data, "  > Loaded: ".. v["data"]["name"])
-						else 
-							_tridentlib.ConsolePrint(data, "  > Loaded: ".. v["raw"])
-						end 
-					end
-				end
-				_tridentlib.ConsolePrint(data, " > Loaded Addon")
-			end
-		end
-	end)
-end                                                                                                                                                                             
